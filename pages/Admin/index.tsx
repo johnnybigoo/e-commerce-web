565b6c46f0c0c75f97293f7b01c8ae3b9eac6ff8
johnnybigoo <jotalico@icloud.com>
2020-12-21 22:15:09 -0500

    Finishing-module 05

---
+++ pages/Admin/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import withAuthAdmin from '../../components/withAuthAdmin';
+import AdminComponent from '../../components/shared/AdminComponent';
+
+const Home: React.FC = () => {
+    return (
+        <AdminComponent>
+                <h1>Parabéns, você acessou o painel! \o/</h1>
+        </AdminComponent>
+    )
+}
+
+export default withAuthAdmin(Home);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,28 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../store';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure()
 
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return(
   <>
-    <Head>
-      <title>OneBitGames</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <title>OneBitGames</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
 
-    <Component {...pageProps} />
+        <Component {...pageProps} />
+        </PersistGate>
+    </Provider>
   </>
   )
 }
