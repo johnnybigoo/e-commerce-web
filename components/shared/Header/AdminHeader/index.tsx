9e63155b963dbe6a29ded008e1e8c6f7311e07da
johnnybigoo <jotalico@icloud.com>
2021-01-20 23:14:10 -0500

    listando-form-categorias

--- components/shared/Header/AdminHeader/index.tsx
+++ components/shared/Header/AdminHeader/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import styles from '../../../../styles/AdminHeader.module.css';
+import styles from '../../../styles/AdminHeader.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignal, faUser, faGamepad, faCheckSquare, faLaptop, faTicketAlt, faDollarSign, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
@@ -12,7 +12,7 @@ interface AdminHeaderProps {
 const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
     return (
         <Row className={styles.background}>
-            <Col lg={6} xs>
+            <Col lg={6} xs={9}>
                 <Link href="/Admin"><a><FontAwesomeIcon icon={faSignal} color="var(--color-gray-light)" className="ml-3" /></a></Link>
                 <Link href="/Admin/Users/List"><a><FontAwesomeIcon icon={faUser} color="var(--color-gray-light)" className="ml-3" /></a></Link>
                 <Link href="/Admin/Products/List"><a><FontAwesomeIcon icon={faGamepad} color="var(--color-gray-light)" className="ml-3" /></a></Link>
@@ -22,7 +22,7 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({ name }) => {
                 <Link href="#"><a><FontAwesomeIcon icon={faDollarSign} color="var(--color-gray-light)" className="ml-3" /></a></Link>
             </Col>
 
-            <Col lg={6} xs>
+            <Col lg={6} xs={3} className="d-none d-md-block">
                 <div className="float-right">
                     <span className={styles.name}>{name}</span>
                     <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
--- components/shared/LateralMenu/index.tsx
+++ components/shared/LateralMenu/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Logo from '../Logo';
 import styles from '../../../styles/MenuLateral.module.css';
-
+import Logo from '../Logo';
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignal, faUser, faGamepad, faCheckSquare, faLaptop, faTicketAlt, faDollarSign, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import Link from 'next/link';
 
 const LateralMenu: React.FC = () => {
     return (
@@ -15,64 +14,64 @@ const LateralMenu: React.FC = () => {
                 <Link href="/Admin">
                     <a>
                         <FontAwesomeIcon icon={faSignal} color="var(--color-gray-light)" className="mr-3" />
-                        Painel Inicial
-                        <hr />
+                            Painel Inicial
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/Users/List">
                     <a>
                         <FontAwesomeIcon icon={faUser} color="var(--color-gray-light)" className="mr-3" />
-                        Usuários
-                        <hr />
+                            Usuários
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/Products/List">
                     <a>
                         <FontAwesomeIcon icon={faGamepad} color="var(--color-gray-light)" className="mr-3" />
-                        Produtos
-                        <hr />
+                            Produtos
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/Categories/List">
                     <a>
                         <FontAwesomeIcon icon={faCheckSquare} color="var(--color-gray-light)" className="mr-3" />
-                        Categorias
-                        <hr />
+                            Categorias
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/SystemRequirements/List">
                     <a>
                         <FontAwesomeIcon icon={faLaptop} color="var(--color-gray-light)" className="mr-3" />
-                        Requisitos do sistema
-                        <hr />
+                            Requisitos do sistema
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/Coupons/List">
                     <a>
                         <FontAwesomeIcon icon={faTicketAlt} color="var(--color-gray-light)" className="mr-3" />
-                        Cupons
-                        <hr />
+                            Cupons
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/#">
                     <a>
                         <FontAwesomeIcon icon={faDollarSign} color="var(--color-gray-light)" className="mr-3" />
-                        Financeiro
-                        <hr />
+                            Financeiro
+                            <hr />
                     </a>
                 </Link>
 
                 <Link href="/Admin/#">
                     <a>
                         <FontAwesomeIcon icon={faSignOutAlt} color="var(--color-gray-light)" className="mr-3" />
-                        Sair
-                        <hr />
+                            Sair
+                            <hr />
                     </a>
                 </Link>
             </div>
