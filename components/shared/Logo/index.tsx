f2da858a7c9ec5a284162aa5a849017da7763a58
johnnybigoo <jotalico@icloud.com>
2021-02-26 19:30:04 -0500

    Adicionando a pagina de pesquisa

--- components/shared/Logo/index.tsx
+++ components/shared/Logo/index.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 import styles from './styles.module.css';
 
 const Logo: React.FC = () => {
-    return (
-        <Link href="/">
-            <a className={styles.logo}>
-                <Image src="/assets/logo-games.png" alt="Logo OneBitGames" width={220} height={40} />
-            </a>
-        </Link>
-    )
+	return (
+		<Link href="/">
+			<a className={styles.logo}>
+				<Image src="/assets/logo-games.png" alt="Logo OneBitGames" width={220} height={40} />
+			</a>
+		</Link>
+	)
 }
 
 export default Logo;
