a1993622bfbd02daee1d4c4258dc88fe4eb7efdf
johnnybigoo <jotalico@icloud.com>
2021-02-27 11:52:31 -0500

    Adicionando filtro para os termos de pesquisa

--- util/ProductSearchService.ts
+++ util/ProductSearchService.ts
@@ -1,46 +1,47 @@
+// Adicionando filtro para os termos de pesquisa
 interface ProductSearchServiceParams {
-  search: string | string[];
-  category?: string | string[];
-  price?: string | string[];
-  page?: string | string[];
-  order?: string | string[];
-  direction?: string | string[];
+	search: string | string[];
+	category?: string | string[];
+	price?: string | string[];
+	page?: string | string[];
+	order?: string | string[];
+	direction?: string | string[];
 }
 
 const ProductSearchService = {
-  execute({
-    search,
-    category,
-    price,
-    page,
-    order = 'price',
-    direction = 'asc'
-  }: ProductSearchServiceParams): string {
-    let returnStr = `?length=12&order[${order}]=${direction}`;
+	execute({
+		search,
+		category,
+		price,
+		page,
+		order = 'price',
+		direction = 'asc'
+	}: ProductSearchServiceParams): string {
+		let returnStr = `?length=12&order[${order}]=${direction}`;
 
-    if (search) {
-      returnStr += `&search=${search}`;
-    }
+		if (search) {
+			returnStr += `&search=${search}`;
+		}
 
-    if (page) {
-      returnStr += `&page=${page}`;
-    }
+		if (page) {
+			returnStr += `&page=${page}`;
+		}
 
-    if (category) {
-      returnStr += `&category_ids[]=${category}`;
-    }
+		if (category) {
+			returnStr += `&category_ids[]=${category}`;
+		}
 
-    if (price) {
-      let prices = price.toString().split('-');
-      returnStr += `&price[min]=${prices[0]}`;
+		if (price) {
+			let prices = price.toString().split('-');
+			returnStr += `&price[min]=${prices[0]}`;
 
-      if (prices.length === 2) {
-        returnStr += `&price[max]=${prices[1]}`;
-      }
-    }
+			if (prices.length === 2) {
+				returnStr += `&price[max]=${prices[1]}`;
+			}
+		}
 
-    return returnStr;
-  }
+		return returnStr;
+	}
 }
 
 export default ProductSearchService;
