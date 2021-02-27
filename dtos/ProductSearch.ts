ee43c989b35a19151075fc97ab5e493873953f12
johnnybigoo <jotalico@icloud.com>
2021-02-27 11:34:38 -0500

    Adicionando service de pesquisa

--- dtos/ProductSearch.ts
+++ dtos/ProductSearch.ts
@@ -1,6 +1,7 @@
+// Adicionando service de pesquisa
 import Product from './Product';
 
-type ProductSearch = 
-  Pick<Product, 'id' | 'name' | 'description' | 'price' | 'image_url' | 'categories'>;
+type ProductSearch =
+	Pick<Product, 'id' | 'name' | 'description' | 'price' | 'image_url' | 'categories'>;
 
 export default ProductSearch;
--- services/search.ts
+++ services/search.ts
@@ -1,17 +1,18 @@
+// Adicionando service de pesquisa
 import api from './api';
 
 import ProductSearch from '../dtos/ProductSearch';
 import Meta from '../dtos/Meta';
 
 interface SearchIndexData {
-  products: ProductSearch[];
-  meta: Meta;
+	products: ProductSearch[];
+	meta: Meta;
 }
 
 const SearchService = {
-  execute: (url: string) => {
-    return api.get<SearchIndexData>(url).then(response => response.data);
-  }
+	execute: (url: string) => {
+		return api.get<SearchIndexData>(url).then(response => response.data);
+	}
 }
 
 export default SearchService;
