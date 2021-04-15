85b14298a938d9c9438782325d2c8957e1910b55
johnnybigoo <jotalico@icloud.com>
2021-04-15 10:38:08 -0400

    Adicionando o callback no login

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -16,99 +16,103 @@ import AuthState from '../../dtos/AuthState';
 import User from '../../dtos/User';
 
 interface LoginProps {
-    titlePhrase: string;
-    buttonPhrase: string;
+	titlePhrase: string;
+	buttonPhrase: string;
 }
 
 const LoginForm: React.FC<LoginProps> = ({ titlePhrase, buttonPhrase }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const passwordRef = useRef(null);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const passwordRef = useRef(null);
 
-    const loggedUser: User = useSelector((state: AuthState) => state.auth.loggedUser);
+	const loggedUser: User = useSelector((state: AuthState) => state.auth.loggedUser);
 
-    const router = useRouter();
-    const dispatch = useDispatch();
+	const router = useRouter();
+	const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (loggedUser) {
-            setEmail(loggedUser.email);
-            if (passwordRef && passwordRef.current) {
-                passwordRef.current.focus();
-            }
-        }
-    }, [loggedUser])
+	useEffect(() => {
+		if (loggedUser) {
+			setEmail(loggedUser.email);
+			if (passwordRef && passwordRef.current) {
+				passwordRef.current.focus();
+			}
+		}
+	}, [loggedUser])
 
-    const handleSubmit = async (evt: React.FormEvent): Promise<void> => {
-        evt.preventDefault();
+	const handleSubmit = async (evt: React.FormEvent): Promise<void> => {
+		evt.preventDefault();
 
-        try {
-            const response = await UsersService.signIn({ email, password });
+		try {
+			const response = await UsersService.signIn({ email, password });
 
-            const { id, email: userEmail, name, profile } = response.data.data;
+			const { id, email: userEmail, name, profile } = response.data.data;
 
-            const user = {
-                id,
-                name,
-                email: userEmail,
-                profile: profile
-            };
+			const user = {
+				id,
+				name,
+				email: userEmail,
+				profile: profile
+			};
 
-            dispatch(setLoggedUser(user));
+			dispatch(setLoggedUser(user));
 
-            toast.info('Login realizado com sucesso!');
+			toast.info('Login realizado com sucesso!');
 
-            router.push(user.profile === 'admin' ? '/Admin' : '/')
-        } catch (err) {
-            toast.error('E-mail ou senha inválidos!');
-        }
-    }
-    return (
+			if (router.query.callback) {
+				router.push(decodeURIComponent(router.query.callback.toString()));
+			} else {
+				router.push(user.profile === 'admin' ? '/Admin' : '/')
+			}
+		} catch (err) {
+			toast.error('E-mail ou senha inválidos!');
+		}
+	}
+	return (
 
-        <form onSubmit={handleSubmit}>
-            <Row>
-                <Col lg={{ span: 6, offset: 3 }} md={{ span: 8, offset: 2 }}>
-                    <BlueBackground>
-                        <h4>{titlePhrase}</h4>
+		<form onSubmit={handleSubmit}>
+			<Row>
+				<Col lg={{ span: 6, offset: 3 }} md={{ span: 8, offset: 2 }}>
+					<BlueBackground>
+						<h4>{titlePhrase}</h4>
 
 
-                        <InputGroup className="mt-3">
-                            <FormControl
-                                placeholder="Meu e-mail"
-                                value={email}
-                                type="email"
-                                onChange={
-                                    (evt: React.ChangeEvent<HTMLInputElement>) =>
-                                        setEmail(evt.target.value)
-                                }
-                                required
-                            />
-                        </InputGroup>
+						<InputGroup className="mt-3">
+							<FormControl
+								placeholder="Meu e-mail"
+								value={email}
+								type="email"
+								onChange={
+									(evt: React.ChangeEvent<HTMLInputElement>) =>
+										setEmail(evt.target.value)
+								}
+								required
+							/>
+						</InputGroup>
 
-                        <InputGroup className="mt-3">
-                            <FormControl
-                                placeholder="Senha"
-                                value={password}
-                                type="password"
-                                onChange={
-                                    (evt: React.ChangeEvent<HTMLInputElement>) =>
-                                        setPassword(evt.target.value)
-                                }
-                                required
-                                ref={passwordRef}
-                            />
-                        </InputGroup>
+						<InputGroup className="mt-3">
+							<FormControl
+								placeholder="Senha"
+								value={password}
+								type="password"
+								onChange={
+									(evt: React.ChangeEvent<HTMLInputElement>) =>
+										setPassword(evt.target.value)
+								}
+								required
+								ref={passwordRef}
+							/>
+						</InputGroup>
 
-                        <Button type="submit" className="btn btn-info mt-3 w-100">{buttonPhrase}</Button>
+						<Button type="submit" className="btn btn-info mt-3 w-100">{buttonPhrase}</Button>
 
-                        <br />
+						<br />
 
-                        <Link href="/Auth/PasswordRecovery">Esqueci minha senha</Link> <br />
-                    </BlueBackground>
-                </Col>
-            </Row>
-        </form>
-    )
+						<Link href="/Auth/PasswordRecovery">Esqueci minha senha</Link> <br />
+					</BlueBackground>
+				</Col>
+			</Row>
+		</form>
+	)
 }
 
 export default LoginForm;
--- services/api.ts
+++ services/api.ts
@@ -8,69 +8,76 @@ import Router from 'next/router';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+	baseURL: 'http://localhost:3000'
 });
 
 // adição da função para setar os headers de authẽnticação na api e nos cookies do browser, iremos utilizar ela no interceptor de request (tanto no fluxo normal quando no fluxo de erro).
 function setHeaders(res: AxiosResponse<any>) {
-    if (res.headers['access-token'] && res.headers['access-token'] !== '') {
-        const apiData: ApiData = {
-            'access-token': res.headers['access-token'],
-            client: res.headers.client,
-            expiry: res.headers.expiry,
-            'token-type': res.headers['token-type'],
-            uid: res.headers.uid
-        };
+	if (res.headers['access-token'] && res.headers['access-token'] !== '') {
+		const apiData: ApiData = {
+			'access-token': res.headers['access-token'],
+			client: res.headers.client,
+			expiry: res.headers.expiry,
+			'token-type': res.headers['token-type'],
+			uid: res.headers.uid
+		};
 
-        api.defaults.headers = apiData;
-        Cookie.set('@api-data', apiData);
-    }
+		api.defaults.headers = apiData;
+		Cookie.set('@api-data', apiData);
+	}
 }
 
 api.interceptors.response.use(res => {
-    setHeaders(res);
-    return res;
+	setHeaders(res);
+	return res;
 }
-    , err => {
-        // caso um erro ocorra na response, um novo token é retornado, logo devemos atualizá-lo na api e nos cookies
-        if (err.response) {
-            setHeaders(err.response);
+	, err => {
+		// caso um erro ocorra na response, um novo token é retornado, logo devemos atualizá-lo na api e nos cookies
+		if (err.response) {
+			setHeaders(err.response);
 
-            const data = err.response.data;
+			const data = err.response.data;
 
-            // aqui estamos tratando os erros no padrão que o rails no devolve, se existem algum array de erros, iremos extrair o nome do campo e as mensagens para que as mesmas possam ser exibidas na tela utilizando um toast
-            if (data && data.errors && data.errors.fields) {
-                const errors = data.errors as ApiResponseError;
+			// aqui estamos tratando os erros no padrão que o rails no devolve, se existem algum array de erros, iremos extrair o nome do campo e as mensagens para que as mesmas possam ser exibidas na tela utilizando um toast
+			if (data && data.errors && data.errors.fields) {
+				const errors = data.errors as ApiResponseError;
 
-                const fieldsName = Object.keys(errors.fields)
+				const fieldsName = Object.keys(errors.fields)
 
-                fieldsName.forEach(error => {
-                    toast.error(error + ': ' + errors.fields[error].join(`, `))
-                })
+				fieldsName.forEach(error => {
+					toast.error(error + ': ' + errors.fields[error].join(`, `))
+				})
 
-                console.log('errors', errors);
-            }
-        }
+				console.log('errors', errors);
+			}
+		}
 
-        // caso a response tenha um status de não autorizado ou acesso negado, o usuário será redirecionado para o login.
-        if (err.response && (
-            err.response.status === 401 ||
-            err.response.status === 403
-        )) {
-            Router.push('/Auth/Login');
-        }
+		// caso a response tenha um status de não autorizado ou acesso negado, o usuário será redirecionado para o login.
+		if (err.response && (
+			err.response.status === 401 ||
+			err.response.status === 403
+		)) {
+			Router.push('/Auth/Login');
+		}
 
-        throw err;
-    });
+		throw err;
+	});
 
 api.interceptors.request.use(req => {
-    req.headers = { ContentType: 'application/json' };
-    if (req.url.includes('admin')) {
-        const apiData: ApiData = JSON.parse(Cookie.get('@api-data'));
-        req.headers = { ...apiData, ...req.headers };
-    }
+	req.headers = { ContentType: 'application/json' };
 
-    return req;
+	if (req.url.includes('admin')) {
+		const apiDataCookie = Cookie.get('@api-data');
+
+		if (!apiDataCookie) {
+			return req;
+		}
+
+		const apiData: ApiData = JSON.parse(apiDataCookie);
+		req.headers = { ...apiData, ...req.headers };
+	}
+
+	return req;
 })
 
 export default api;
