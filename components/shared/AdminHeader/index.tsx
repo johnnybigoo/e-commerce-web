1528126b9d87b4079651589a810500ae73371740
johnnybigoo <jotalico@icloud.com>
2021-04-30 09:54:04 -0400

    Adicionando rotas para o profile 1

--- components/shared/AdminHeader/index.tsx
+++ components/shared/AdminHeader/index.tsx
@@ -12,96 +12,97 @@ import User from '../../../dtos/User';
 import SignOutService from '../../../util/SignOutService';
 
 const AdminHeader: React.FC = () => {
-    const router = useRouter();
+	const router = useRouter();
 
-    const { name }: User = useSelector(state => state.auth.loggedUser);
+	const { name }: User = useSelector(state => state.auth.loggedUser);
 
-    return (
-        <Row className={styles.background}>
-            <Col lg={6} xs={12} className={styles.menu}>
-                <Link href="/Admin">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faSignal}
-                            color="var(--color-gray-light)"
-                            className={`ml-3 ${router.pathname === '/Admin' ? styles.active : ''}`}
-                        />
-                    </a>
-                </Link>
-                <Link href="/Admin/Users/List">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faUser}
-                            color="var(--color-gray-light)"
-                            className={`ml-3 ${router.pathname === '/Admin/Users/List' ? styles.active : ''}`}
-                        />
-                    </a>
-                </Link>
-                <Link href="/Admin/Products/List">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faGamepad}
-                            color="var(--color-gray-light)"
-                            className={`ml-3 ${router.pathname === '/Admin/Products/List' ? styles.active : ''}`}
-                        />
-                    </a>
-                </Link>
-                <Link href="/Admin/Categories/List">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faCheckSquare}
-                            color="var(--color-gray-light)"
-                            className={`ml-3 ${router.pathname === '/Admin/Categories/List' ? styles.active : ''}`}
-                        />
-                    </a>
-                </Link>
-                <Link href="/Admin/SystemRequirements/List">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faLaptop}
-                            color="var(--color-gray-light)"
-                            className={`ml-3 ${router.pathname === '/Admin/SystemRequirements/List' ? styles.active : ''}`}
-                        />
-                    </a>
-                </Link>
-                <Link href="/Admin/Coupons/List">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faTicketAlt}
-                            color="var(--color-gray-light)"
-                            className={`ml-3 ${router.pathname === '/Admin/Coupons/List' ? styles.active : ''}`}
-                        />
-                    </a>
-                </Link>
+	return (
+		<Row className={styles.background}>
+			<Col xs={12} className={styles.menu}>
+				<Link href="/Admin">
+					<a>
+						<FontAwesomeIcon
+							icon={faSignal}
+							color="var(--color-gray-light)"
+							className={`ml-3 ${router.pathname === '/Admin' ? styles.active : ''}`}
+						/>
+					</a>
+				</Link>
+				<Link href="/Admin/Users/List">
+					<a>
+						<FontAwesomeIcon
+							icon={faUser}
+							color="var(--color-gray-light)"
+							className={`ml-3 ${router.pathname === '/Admin/Users/List' ? styles.active : ''}`}
+						/>
+					</a>
+				</Link>
+				<Link href="/Admin/Products/List">
+					<a>
+						<FontAwesomeIcon
+							icon={faGamepad}
+							color="var(--color-gray-light)"
+							className={`ml-3 ${router.pathname === '/Admin/Products/List' ? styles.active : ''}`}
+						/>
+					</a>
+				</Link>
+				<Link href="/Admin/Categories/List">
+					<a>
+						<FontAwesomeIcon
+							icon={faCheckSquare}
+							color="var(--color-gray-light)"
+							className={`ml-3 ${router.pathname === '/Admin/Categories/List' ? styles.active : ''}`}
+						/>
+					</a>
+				</Link>
+				<Link href="/Admin/SystemRequirements/List">
+					<a>
+						<FontAwesomeIcon
+							icon={faLaptop}
+							color="var(--color-gray-light)"
+							className={`ml-3 ${router.pathname === '/Admin/SystemRequirements/List' ? styles.active : ''}`}
+						/>
+					</a>
+				</Link>
+				<Link href="/Admin/Coupons/List">
+					<a>
+						<FontAwesomeIcon
+							icon={faTicketAlt}
+							color="var(--color-gray-light)"
+							className={`ml-3 ${router.pathname === '/Admin/Coupons/List' ? styles.active : ''}`}
+						/>
+					</a>
+				</Link>
 
-                <Link href="#">
-                    <a>
-                        <FontAwesomeIcon
-                            icon={faDollarSign}
-                            color="var(--color-gray-light)"
-                            className="ml-3" />
-                    </a>
-                </Link>
+				<Link href="#">
+					<a>
+						<FontAwesomeIcon
+							icon={faDollarSign}
+							color="var(--color-gray-light)"
+							className="ml-3" />
+					</a>
+				</Link>
 
-                <Link href="/Auth/Login">
-                    <a
-                        onClick={SignOutService.execute}
-                        onTouchEnd={() => SignOutService.execute()}
-                    >
-                        <FontAwesomeIcon
-                            icon={faSignOutAlt}
-                            color="var(--color-gray-light)"
-                            className="ml-3" />
-                    </a>
-                </Link>
-            </Col>
-
-            <Col lg={6} xs={3} className={styles.profile}>
-                <span className={styles.name}>{name}</span>
-                <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
-            </Col>
-        </Row>
-    )
+				<Link href="/Auth/Login">
+					<a
+						onClick={SignOutService.execute}
+						onTouchEnd={() => SignOutService.execute()}
+					>
+						<FontAwesomeIcon
+							icon={faSignOutAlt}
+							color="var(--color-gray-light)"
+							className="ml-3" />
+					</a>
+				</Link>
+				<Link href="/Profile">
+					<a className={styles.profile}>
+						<span className={styles.name}>{name}</span>
+						<FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
+					</a>
+				</Link>
+			</Col>
+		</Row>
+	)
 }
 
 export default AdminHeader;
